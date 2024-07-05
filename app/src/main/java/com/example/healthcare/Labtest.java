ce0683a01ebf580c2425b6457309f7510fd1342c
Your Name <nurtajnasrin@gamil.com>
2024-07-05 21:00:49 +0400

    other list added

--- app/src/main/java/com/example/healthcare/Labtest.java
+++ app/src/main/java/com/example/healthcare/Labtest.java
@@ -1,6 +1,8 @@
 package com.example.healthcare;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Labtest extends AppCompatActivity {
+    private String[] labTestNames = {
+            "Complete Blood Count (CBC)",
+            "Basic Metabolic Panel (BMP)",
+            "Lipid Panel",
+            "Thyroid Stimulating Hormone (TSH)",
+            "Urinalysis (UA)"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_labtest);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ListView listView = findViewById(R.id.labListView);
+
+        // Create an ArrayAdapter to populate the ListView with doctor names
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, labTestNames);
+
+        // Set the ArrayAdapter to the ListView
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/healthcare/Medicine.java
+++ app/src/main/java/com/example/healthcare/Medicine.java
@@ -1,6 +1,8 @@
 package com.example.healthcare;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Medicine extends AppCompatActivity {
 
+    private String[] medicineNames = {
+            "Acetaminophen",
+            "Ibuprofen",
+            "Aspirin",
+            "Lisinopril",
+            "Levothyroxine",
+            "Amlodipine",
+            "Metformin",
+            "Atorvastatin",
+            "Simvastatin",
+            "Metoprolol",
+            "Omeprazole",
+            "Losartan",
+            "Albuterol",
+            "Azithromycin",
+            "Amoxicillin",
+            "Prednisone",
+            "Citalopram",
+            "Escitalopram",
+            "Sertraline",
+            "Fluoxetine",
+            "Hydrochlorothiazide",
+            "Gabapentin",
+            "Tramadol",
+            "Warfarin",
+            "Ranitidine",
+            "Pantoprazole",
+            "Doxycycline",
+            "Clindamycin",
+            "Cephalexin",
+            "Amphetamine",
+            "Diazepam",
+            "Oxycodone",
+            "Morphine",
+            "Furosemide",
+            "Metronidazole",
+            "Bupropion",
+            "Lorazepam",
+            "Venlafaxine",
+            "Trazodone",
+            "Phenytoin"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_medicine);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ListView listView = findViewById(R.id.medicine);
+
+        // Create an ArrayAdapter to populate the ListView with doctor names
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, medicineNames);
+
+        // Set the ArrayAdapter to the ListView
+        listView.setAdapter(adapter);
     }
 }
