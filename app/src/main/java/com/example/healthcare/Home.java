7929c13a7caf3e7b4d96a38885dd9c972fc75a67
Your Name <nurtajnasrin@gamil.com>
2024-07-05 20:15:02 +0400

    added some picture and page

--- app/src/main/java/com/example/healthcare/Home.java
+++ app/src/main/java/com/example/healthcare/Home.java
@@ -16,12 +16,63 @@ import java.security.Signature;
 
 public class Home extends AppCompatActivity {
 
-
+TextView lab, doc,medicine,order,articales;
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        lab=findViewById(R.id.lab_text);        lab=findViewById(R.id.lab_text);
+        doc=findViewById(R.id.doctor_text);
+        medicine=findViewById(R.id.medicine_text);
+        order=findViewById(R.id.order_text);
+
+        articales=findViewById(R.id.articals_text);
+
+        lab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this, Labtest.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this, doctor.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        medicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this, Medicine.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        articales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this, health_articales.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this, order_details.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
 
     }
     }
